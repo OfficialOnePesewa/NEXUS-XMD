b44b4dfcbe0797db19bdc49913de09f50d3b7b3b
OfficialOnePesewa <153733681+OfficialOnePesewa@users.noreply.github.com>
2025-04-27 14:49:51 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PkdrilleroXl0EDzK#CB-UfHwT1CEIRNLhQvABKuh1kwTvusopnHFIvjIDG6c",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
